1fef2563b1397aa718014ab9c18d120056542548
Aaron <aaronkcarpenter@gmail.com>
2020-09-08 18:50:15 -0500

    Firebase install firebase tools install

--- capstone-app/src/index.js
+++ capstone-app/src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as firebase from 'firebase/app';
+import 'firebase/analytics';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyCGtpXvKcsOpfhF_sRrhB4dQhoaKio89ds",
+    authDomain: "app-academy-capstone-project.firebaseapp.com",
+    databaseURL: "https://app-academy-capstone-project.firebaseio.com",
+    projectId: "app-academy-capstone-project",
+    storageBucket: "app-academy-capstone-project.appspot.com",
+    messagingSenderId: "175261622378",
+    appId: "1:175261622378:web:5edc8b65c2f3d1dcdbea13",
+    measurementId: "G-BKYJ0WYRRN"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +26,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
